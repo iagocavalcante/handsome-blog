106ef7ee19fcfa34b38c0e64a3dd921be32a849b
Iago Angelim Cavalcante <iagoangelimc@gmail.com>
2018-09-28 22:18:39 -0300

    Component para renderizar o portfolio

--- src/domains/Portfolio/Components/Portfolio.vue
+++ src/domains/Portfolio/Components/Portfolio.vue
@@ -1,60 +1,17 @@
 <template>
-  <!-- Work Detail Section -->
   <section class="ptb ptb-sm-80">
     <div class="container">
   <!-- End work Filter -->
-      <div class="row container-grid nf-col-2">
+      <div class="row container-grid nf-col-3">
 
-        <div class="nf-item branding coffee spacing">
+        <div class="nf-item branding coffee spacing" :key="index" v-for="(port, index) in portfolio">
           <div class="item-box">
-            <a href="portfolio-single1.html">
-              <img alt="1" src="https://image.slidesharecdn.com/viverdeblogebooktesteab-170711002058/95/viver-de-blog-e-book-teste-ab-1-638.jpg?cb=1499732905" class="item-container">
+            <a :href="port.link">
+              <img alt="1" :src="port.image" class="item-container">
               <div class="item-mask">
                 <div class="item-caption">
-                  <h5 class="white">Consequat massa quis</h5>
-                  <p class="white">Branding, Design</p>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        
-        <div class="nf-item branding coffee spacing">
-          <div class="item-box">
-            <a href="portfolio-single1.html">
-              <img alt="1" src="https://image.slidesharecdn.com/viverdeblogebooktesteab-170711002058/95/viver-de-blog-e-book-teste-ab-1-638.jpg?cb=1499732905" class="item-container">
-              <div class="item-mask">
-                <div class="item-caption">
-                  <h5 class="white">Consequat massa quis</h5>
-                  <p class="white">Branding, Design</p>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        
-        <div class="nf-item branding coffee spacing">
-          <div class="item-box">
-            <a href="portfolio-single1.html">
-              <img alt="1" src="https://image.slidesharecdn.com/viverdeblogebooktesteab-170711002058/95/viver-de-blog-e-book-teste-ab-1-638.jpg?cb=1499732905" class="item-container">
-              <div class="item-mask">
-                <div class="item-caption">
-                  <h5 class="white">Consequat massa quis</h5>
-                  <p class="white">Branding, Design</p>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        
-        <div class="nf-item branding coffee spacing">
-          <div class="item-box">
-            <a href="portfolio-single1.html">
-              <img alt="1" src="https://image.slidesharecdn.com/viverdeblogebooktesteab-170711002058/95/viver-de-blog-e-book-teste-ab-1-638.jpg?cb=1499732905" class="item-container">
-              <div class="item-mask">
-                <div class="item-caption">
-                  <h5 class="white">Consequat massa quis</h5>
-                  <p class="white">Branding, Design</p>
+                  <h5 class="white">{{port.name}}</h5>
+                  <p class="white">{{port.type}}</p>
                 </div>
               </div>
             </a>
@@ -67,8 +24,67 @@
 </template>
 
 <script>
+import Chopp from '@/assets/chopp.png'
+import Conceito from '@/assets/conceito.png'
+import i9Empresa from '@/assets/i9empresa.jpeg'
+import i9Gestao from '@/assets/i9gestao.jpeg'
+import i9Site from '@/assets/i9site.jpeg'
+import OuuaProd from '@/assets/ouua_prod.png'
+import Ouua from '@/assets/ouua.png'
+import Vila from '@/assets/vila.jpeg'
 export default {
-
+  data: () => ({
+    portfolio: [
+      {
+        image: Chopp,
+        name: 'Chopp Delivery',
+        type: 'Layout, Site (Desativado)',
+        link: '#'
+      },
+      {
+        image: Conceito,
+        name: 'Conceito Representação',
+        type: 'Layout, Site (Desativado)',
+        link: '#'
+      },
+      {
+        image: i9Empresa,
+        name: 'Sistema i9Empresa',
+        type: 'ERP',
+        link: '#'
+      },
+      {
+        image: i9Gestao,
+        name: 'i9 Gestão',
+        type: 'Dashboard',
+        link: 'http://i9gestao.i9amazon.com.br/'
+      },
+      {
+        image: i9Site,
+        name: 'i9 Amazon',
+        type: 'Dashboard Cliente, Site (Atualmente modificado)',
+        link: 'http://i9amazon.com.br/'
+      },
+      {
+        image: Ouua,
+        name: 'Ouua Painel',
+        type: 'Dashboard',
+        link: 'http://painel.ouua.com.br/'
+      },
+      {
+        image: OuuaProd,
+        name: 'Ouua App',
+        type: 'Android',
+        link: 'http://ouua.com.br/'
+      },
+      {
+        image: Vila,
+        name: 'Vila do Silício',
+        type: 'Site, Blog',
+        link: 'http://viladosilicio.com.br/'
+      }
+    ]
+  })
 }
 </script>
 
