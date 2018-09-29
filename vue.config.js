fe02223a05748370a85f8d90c25358e9836a166f
Iago Angelim Cavalcante <iagoangelimc@gmail.com>
2018-09-28 22:18:53 -0300

    Image loader

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@ module.exports = {
   pwa: {
     name: 'Iago Cavalcante',
     themeColor: '#050505'
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => Object.assign(options, { limit: 10240 }))
   }
 }
